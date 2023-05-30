9330780445e3234c03c28ef225840dbf91e759ff
mandouh19 <mandouh1972@gmail.com>
2023-05-30 18:30:17 +0300

    Update navsite.js

--- js/navsite.js
+++ js/navsite.js
@@ -4,9 +4,14 @@ $("#navbarCollapse").html('');
 		dataType: 'json',		
 		success: function (data) {
 			var items = '';
-           
+           var no=0;
 			$.each(data, function (i, row) {
-                items +=' <a href="'+row.Url+'" class="nav-item nav-link">'+row.Name+'</a>';				
+                if(no==0){
+                    items +=' <a href="'+row.Url+'" class="nav-item nav-link active">'+row.Name+'</a>';	
+                }else{
+                    items +=' <a href="'+row.Url+'" class="nav-item nav-link">'+row.Name+'</a>';	
+                }
+                no++;
 			});
 			$("#navbarCollapse").html(items);
 		}
